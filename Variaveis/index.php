e83becf67210713b3277759174379342810b4538
Pablo Riquelme Santana De Souza <105436418+riquelmexs@users.noreply.github.com>
2024-04-13 01:04:08 -0300

    Add files via upload

---
+++ Variaveis/index.php
@@ -0,0 +1,27 @@
+<?php
+
+  /*variaveis */
+
+   // $1var = 0;
+   // $!var = o;
+   // $variavel(1) = 0;
+   // $variavel1 um = 0;
+   // $variavel1-um = 0;
+ 
+
+   $var1 = 0;
+   $var_um = 0;
+   $varUm = 0;
+   $_um = 0;
+   $_1 = 0;
+
+   # convenções
+   $umaVariavel = 0; # camel case
+   $uma_variavel = 0; # com undersores (snake case)
+   $UmaVariavel = 0; # studly case (não recomendado)
+   
+
+   # DEVE SER SEMPRE MANTIDO O MESMO ESTILO AO LONGO DO PROJETO
+
+   $variavel = 100;
+   echo $variavel;
