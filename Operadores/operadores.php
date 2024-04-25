6632fd179d1e09379f8c46941a083114e6cf25d1
Pablo Riquelme Santana De Souza <105436418+riquelmexs@users.noreply.github.com>
2024-04-25 17:34:04 -0300

    Add files via upload

---
+++ Operadores/operadores.php
@@ -0,0 +1,36 @@
+<?php
+
+# Operadores
+# ======================================
+# São símbolos que permitem executar operações matemáticas
+# ou operações lógicas / comparativas
+
+
+# operador de atribuição (assignment)
+$variavel = 100;
+
+$variavel ='joao';
+
+# operadores aritméticos (para execução de cálculos)
+$x = 4 + 2; #adição
+$x = 4 - 2; #subtração
+$x = 4 * 2; #multiplicação
+$x = 4 / 2; #divisão
+$x = 4 % 2; # modulus (Restante da divisão)
+
+?>
+
+<?php
+
+# modulos
+$x = 100% 3;  # 33.3 ...1
+$x = 60 % 3;  # 20 ... 0
+$x = 13 % 2;  # 6.5 ... 1
+
+?>
+
+<?php
+
+echo 12334 / 4;
+echo '<br>';
+echo 1234 % 6;
