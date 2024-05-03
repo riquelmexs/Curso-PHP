6678da569448e30ffc6c3c6390aafdaabde13f40
Pablo Riquelme Santana De Souza <105436418+riquelmexs@users.noreply.github.com>
2024-05-03 09:44:49 -0300

    Add files via upload

---
+++ Operadores-Atribuição.php
@@ -0,0 +1,17 @@
+<?php
+ # OPERADORES DE ATRIBUIÇÃO CONBINADOS
+ # PERMITEM ALTERAR O VALOR DE UMA VARÍAVEL, A PARTIR DO SEU VALOR ORIGINAL
+
+ $a = 10;
+
+ $a = $a + 20; #30
+ $a = $a - 3;  #7
+ $a = $a * 2;  #20
+ $a = $a / 2;  #5
+
+   //SIMPLIFICADO...
+
+$a += 20;  #30
+$a -= 3;   #7
+$a *=2;    #20
+$a /=2;    #5
